cc205e9a8bbebf3ae8a200703bd42f8dfad9425b
MdAasif06 <abdurm477@gmail.com>
2025-10-11 07:02:07 +0530

    home page created

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Banner from '../components/Home/Banner'
+import Hero from '../components/Home/Hero'
+import Features from '../components/Home/Features'
+import Testimonial from '../components/Home/Testimonial'
+import CaallToAction from '../components/Home/CaallToAction'
+import Footer from '../components/Home/Footer'
 
 const Home = () => {
   return (
-    <div>Home page</div>
+    <div>
+      <Banner/>
+      <Hero/>
+      <Features/>
+      <Testimonial/>
+      <CaallToAction/>
+      <Footer/>
+    </div>
   )
 }
 
