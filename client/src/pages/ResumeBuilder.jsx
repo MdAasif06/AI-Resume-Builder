e17a79da835887c23745f1c158c3c876d7eeff2d
MdAasif06 <abdurm477@gmail.com>
2025-10-15 22:37:09 +0530

    create a persoanl dummay data component

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -1,7 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { dummyResumeData } from "../assets/assets";
 import { Link, useParams } from "react-router-dom";
-import { ArrowLeftIcon, Briefcase, FileText, FolderIcon, GraduationCap, Sparkles, User } from "lucide-react";
+import {
+  ArrowLeftIcon,
+  Briefcase,
+  ChevronLeft,
+  ChevronRight,
+  FileText,
+  FolderIcon,
+  GraduationCap,
+  Sparkles,
+  User,
+} from "lucide-react";
+import PersonalInfo from "../components/PersonalInfo";
 
 const ResumeBuilder = () => {
   const { resumeId } = useParams();
@@ -27,17 +38,17 @@ const ResumeBuilder = () => {
     }
   };
 
-  const [activeSectionIndex,setActiveSectionIndex]=useState(0)
-  const [removeBackground,setRemoveBackground]=useState(false)
-  const sections=[
-    {id:"personal",name:"Personal info",icon:<User/>},
-    {id:"summary",name:"Summary",icon:<FileText/>},
-    {id:"exprience",name:"Expreience",icon:<Briefcase/>},
-    {id:"education",name:"Education",icon:<GraduationCap/>},
-    {id:"projects",name:"Projects",icon:<FolderIcon/>},
-    {id:"skill",name:"Skills",icon:<Sparkles/>},
-  ]
-  const activeSection=sections[activeSectionIndex]
+  const [activeSectionIndex, setActiveSectionIndex] = useState(0);
+  const [removeBackground, setRemoveBackground] = useState(false);
+  const sections = [
+    { id: "personal", name: "Personal info", icon: <User /> },
+    { id: "summary", name: "Summary", icon: <FileText /> },
+    { id: "exprience", name: "Expreience", icon: <Briefcase /> },
+    { id: "education", name: "Education", icon: <GraduationCap /> },
+    { id: "projects", name: "Projects", icon: <FolderIcon /> },
+    { id: "skill", name: "Skills", icon: <Sparkles /> },
+  ];
+  const activeSection = sections[activeSectionIndex];
 
   useEffect(() => {
     loadExistingResume();
@@ -57,22 +68,75 @@ const ResumeBuilder = () => {
       <div className="max-w-7xl mx-auto px-4 pb-8">
         <div className="grid lg:grid-cols-12 gap-8">
           {/* left panel form */}
-           <div className="relative lg:col-span-5 rounded-lg overflow-hidden">
-            <div className="bg-white rounded-lg shadow-md border border-gray-200
-            p-6 pt-1">
+          <div className="relative lg:col-span-5 rounded-lg overflow-hidden">
+            <div
+              className="bg-white rounded-lg shadow-md border border-gray-200
+            p-6 pt-1"
+            >
               {/* progress bar using active section */}
-              <hr className="absolute top-0 left-0 right-0 border-2 border-gray-200"/>
-              <hr className="absolute top-0 left-0 h-1 bg-gradient-to-r from-green-500 to-green-600
-              border-none transition-all duration-200" style={{width:`${activeSectionIndex * 100/(sections.length-1)}%`}} />
-
+              <hr className="absolute top-0 left-0 right-0 border-2 border-gray-200" />
+              <hr
+                className="absolute top-0 left-0 h-1 bg-gradient-to-r from-green-500 to-green-600
+              border-none transition-all duration-200"
+                style={{
+                  width: `${
+                    (activeSectionIndex * 100) / (sections.length - 1)
+                  }%`,
+                }}
+              />
             </div>
-            
-           </div>
+            {/* section navigation */}
+            <div className="flex justify-between items-center mb-6 border-b border-gray-300 py-1">
+              <div></div>
+              <div className="flex items-center">
+                {activeSectionIndex !== 0 && (
+                  <button
+                    onClick={() =>
+                      setActiveSectionIndex(
+                        () => (prevIndex) => Math.max(prevIndex - 1, 0)
+                      )
+                    }
+                    className="flex items-center gap-1 p-3 rounded-lg text-sm font-medium
+                text-gray-600 hover:bg-gray-50 transition-all"
+                    disabled={activeSectionIndex === 0}
+                  >
+                    <ChevronLeft className="size-4" />
+                    Previous
+                  </button>
+                )}
+                <button onClick={() =>
+                    setActiveSectionIndex(() => (prevIndex) =>
+                    Math.min(prevIndex + 1, sections.length - 1))
+                  }
+                  className={`flex items-center gap-1 p-3 rounded-lg
+                  text-sm font-medium text-gray-600 hover:bg-gray-50 transition-all ${
+                  activeSectionIndex === sections.length - 1 && "opacity-50 "
+                }`}
+                  disabled={activeSectionIndex === sections.length - 1}
+                >Next
+                  <ChevronRight className="size-4" />
+                  
+                </button>
+              </div>
+            </div>
+
+            {/* form section  */}
+            <div className="space-y-6">
+              {activeSection.id === "personal" && (
+                <PersonalInfo
+                  data={resumeData.personal_info}
+                  onChange={(data) =>
+                    setResumeData((prev) => ({ ...prev, personal_info: data }))
+                  }
+                  removeBackGround={removeBackground}
+                  setRemoveBackground={setRemoveBackground}
+                />
+              )}
+            </div>
+          </div>
 
           {/* right panel form */}
-          <div>
-
-          </div>
+          <div></div>
         </div>
       </div>
     </div>
